21741aa8a6614e85dcc8f3a1bed100b23cb9f260
Amer Imsic <amer248@live.com>
2017-02-16 12:04:29 -0800

    First commit to github

---
+++ DogWash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DogWash.Startup))]
+namespace DogWash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
