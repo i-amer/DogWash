75afeeb94072dfd5432d98c239e054806861917a
Emily <epaulson@uw.edu>
2017-02-16 14:34:14 -0800

    Updated Home Index.

--- DogWash/Controllers/HomeController.cs
+++ DogWash/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace DogWash.Controllers
 
             return View();
         }
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Your schedule page.";
+
+            return View();
+        }
+
     }
 }
